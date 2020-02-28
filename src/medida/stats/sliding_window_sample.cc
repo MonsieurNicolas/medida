dddaef8f739b1c73505eee21a5f553a03f8a2860
MonsieurNicolas <nicolas@stellar.org>
2020-02-28 09:52:06 -0800

    sliding window: change scheme to pick a random element per time slice

--- src/medida/stats/sliding_window_sample.cc
+++ src/medida/stats/sliding_window_sample.cc
@@ -33,9 +33,10 @@ class SlidingWindowSample::Impl
     const std::size_t windowSize_;
     const std::chrono::seconds windowTime_;
     const std::chrono::microseconds timeSlice_;
-    std::size_t admissionMask_;
+    std::uint32_t sliceRandomizer_;
+    std::uint32_t lastElementHash_;
     std::default_random_engine rng_;
-    std::uniform_int_distribution<size_t> dist_;
+    std::uniform_int_distribution<std::uint32_t> dist_;
     std::deque<std::pair<double, Clock::time_point>> values_;
 };
 
@@ -85,9 +86,12 @@ SlidingWindowSample::Impl::Impl(std::size_t windowSize,
                                 std::chrono::seconds windowTime)
     : windowSize_(windowSize)
     , windowTime_(windowTime)
-    , timeSlice_(std::chrono::duration_cast<std::chrono::microseconds>(windowTime)/windowSize)
-    , admissionMask_(0)
-    , dist_(0, std::numeric_limits<size_t>::max())
+    , timeSlice_(
+          std::chrono::duration_cast<std::chrono::microseconds>(windowTime) /
+          windowSize)
+    , sliceRandomizer_(0)
+    , lastElementHash_(0)
+    , dist_(0, std::numeric_limits<std::uint32_t>::max())
 {
     Clear();
 }
@@ -101,7 +105,6 @@ SlidingWindowSample::Impl::Clear()
 {
     std::lock_guard<std::mutex> lock{mutex_};
     values_.clear();
-    admissionMask_ = 0;
 }
 
 std::uint64_t
@@ -109,6 +112,23 @@ SlidingWindowSample::Impl::size()
 {
     std::lock_guard<std::mutex> lock{mutex_};
     return values_.size();
+}
+
+// NB: hash function used here should have the "avalanche" property
+static uint32_t
+jenkins_one_at_a_time_hash(const uint8_t* key, size_t length, uint32_t hash)
+{
+    size_t i = 0;
+    while (i != length)
+    {
+        hash += key[i++];
+        hash += hash << 10;
+        hash ^= hash >> 6;
+    }
+    hash += hash << 3;
+    hash ^= hash >> 11;
+    hash += hash << 15;
+    return hash;
 }
 
 void
@@ -125,10 +145,11 @@ SlidingWindowSample::Impl::Update(std::int64_t value,
 
     if (!values_.empty())
     {
-        // If we're in a new timeslice, reset the admission mask.
+        // If we're in a new timeslice, change the random order.
         if (timestamp > values_.back().second + timeSlice_)
         {
-            admissionMask_ = 0;
+            sliceRandomizer_ = dist_(rng_);
+            lastElementHash_ = 0;
         }
 
         // If there's old data, trim it.
@@ -142,27 +163,31 @@ SlidingWindowSample::Impl::Update(std::int64_t value,
     // When you add samples to the sliding window _slowly_ nothing goes wrong;
     // when you add them too _quickly_ there's the possibility of losing rare
     // events because they're overwritten before they get observed. To
-    // compensate for this, the sliding window maintains an "admission mask"
-    // that's reset on each timeslice, and used for exponentially-more-forceful
-    // stochastic rate limiting of replacement of a sample within the timeslice.
+    // compensate for this, we pick a "sliceRandomizer_" for each timeslice,
+    // and used use it to generate a pseudo-random order for the samples.
+    // We then pick the biggest value in this random order which
+    // corresponds to a random element from that timeslice.
+    uint32_t h = jenkins_one_at_a_time_hash(reinterpret_cast<uint8_t*>(&value),
+                                            sizeof(value), sliceRandomizer_);
 
-    if ((dist_(rng_) & admissionMask_) == 0)
+    // Check if we've already inserted an item for the same timeSlice.
+    if (!values_.empty() && timestamp <= values_.back().second + timeSlice_)
     {
-        admissionMask_ <<= 1;
-        admissionMask_ |= 1;
-        // Check if we've already inserted an item for the same timeSlice.
-        if (!values_.empty() && timestamp <= values_.back().second + timeSlice_)
+        // now, check if it's a greater value
+        if (h >= lastElementHash_)
         {
             // Keep old timestamp to anchor timeSlice; but replace value.
             values_.back().first = value;
+            lastElementHash_ = h;
         }
-        else
+    }
+    else
+    {
+        values_.emplace_back(value, timestamp);
+        lastElementHash_ = h;
+        if (values_.size() > windowSize_)
         {
-            values_.emplace_back(value, timestamp);
-            if (values_.size() > windowSize_)
-            {
-                values_.pop_front();
-            }
+            values_.pop_front();
         }
     }
 }
